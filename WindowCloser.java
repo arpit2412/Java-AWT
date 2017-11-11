7b05cc7422e9ed1b77ce5a29e6d9de1d2bcb3d1a
Arpit Garg <arpit.rikki2412@gmail.com>
2017-11-12 00:29:05 +0530

    Activating X
    Windows closer by X button

---
+++ WindowCloser.java
@@ -0,0 +1,14 @@
+import java.awt.*;
+import java.awt.event.*;
+
+
+public class WindowCloser extends WindowAdapter
+{
+public void windowClosing(WindowEvent e)
+{
+Window w=e.getWindow();
+w.setVisible(false);
+w.dispose(); 
+//System.exit(1);
+} 
+}
